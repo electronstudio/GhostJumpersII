53308b49dd4f54b8515cccb067a3b10f2c48d460
electronstudio <misc@electronstudio.co.uk>
2018-03-06 21:48:26 +0000

    allow gamefactory to inject a preselected inputdevice

--- core/src/com/mygdx/game/PimpGameFactory.kt
+++ core/src/com/mygdx/game/PimpGameFactory.kt
@@ -1,9 +1,12 @@
 package com.mygdx.game
 
+import com.badlogic.gdx.Application
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Texture
+import uk.me.fantastic.retro.App
 import uk.me.fantastic.retro.games.AbstractGameFactory
 import uk.me.fantastic.retro.games.Game
+import uk.me.fantastic.retro.input.GamepadInput
 import uk.me.fantastic.retro.screens.GameSession
 
 /*
@@ -14,6 +17,12 @@ class PimpGameFactory : AbstractGameFactory("Pimp Game", null) {
     override val image by lazy { Texture(Gdx.files.internal("badlogic.jpg")) }
 
     override fun create(session: GameSession): Game {
+        if(Gdx.app.type==Application.ApplicationType.Desktop) {
+            val controller1 = App.mappedControllers.firstOrNull()
+            if(controller1!=null) {
+                session.preSelectedInputDevice = GamepadInput(controller1)
+            }
+        }
         return PimpGame(session, 1, 0)
     }
 }
