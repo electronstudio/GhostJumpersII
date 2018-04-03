78fd0fc368567f90e17dd855402f15bfdd8f261e
electronstudio <misc@electronstudio.co.uk>
2018-04-03 16:10:42 +0100

    use androidcallback from retrowar jar, change name of launcher so doesnt conflict with launcher in retrowar jar

--- android/src/com/mygdx/game/AndroidLauncher.kt
+++ android/src/com/mygdx/game/AndroidLauncher.kt
@@ -6,8 +6,9 @@ import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 import uk.me.fantastic.retro.AndroidLogger
 import uk.me.fantastic.retro.SimpleApp
+import uk.me.fantastic.retro.android.AndroidCallback
 
-class AndroidLauncher : AndroidApplication() {
+class Launcher : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val callback = AndroidCallback()
 
